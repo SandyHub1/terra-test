5d15facf52222d519ccdd6eec074a69a2463d816
SandyHub1 <114899001+SandyHub1@users.noreply.github.com>
2022-12-15 10:52:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
-# Configure the Microsoft Azure provider
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
-
 # Create a Resource Group if it doesn’t exist
 resource "azurerm_resource_group" "tfexample" {
   name     = "my-terraform-rg"
