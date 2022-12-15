b560d75ea7f1ef382ce331d7919b18b58dc51a89
SandyHub1 <114899001+SandyHub1@users.noreply.github.com>
2022-12-15 11:11:31 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "2.10.0"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  version = "2.10.0"
   features {}
 }
 # Create a Resource Group if it doesn’t exist
